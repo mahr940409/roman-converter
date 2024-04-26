67344567cea5d2c4f2a15b6391b4aafc1b5c3b86
Manuel Henrndez <131784503+mahr940409@users.noreply.github.com>
2024-04-25 19:24:52 -0500

    Actualizacion

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ function convertNumber() {
   }
 
   if (numberInput >= 4000) {
-      outputElement.textContent = "Please enter a number less than or equal to 3999";
+      outputElement.textContent = "Please enter a number less than 4000";
       return;
   }
 
@@ -45,4 +45,51 @@ function convertToRoman(num) {
   }
 
   return roman;
+}
+
+document.getElementById('convert-roman-btn').addEventListener('click', convertRomanNumber);
+
+function convertRomanNumber() {
+  var romanInput = document.getElementById('roman').value.toUpperCase();
+  var outputElement = document.getElementById('output-roman');
+
+  if (!isValidRomanNumeral(romanInput)) {
+      outputElement.textContent = "Please enter a valid Roman numeral";
+      return;
+  }
+
+  outputElement.textContent = convertToNumber(romanInput);
+}
+
+function isValidRomanNumeral(roman) {
+  // Regular expression to validate Roman numerals
+  var romanPattern = /^(M{0,3})(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,3})$/;
+  return romanPattern.test(roman);
+}
+
+function convertToNumber(roman) {
+  var romanToNumber = {
+      "I": 1,
+      "V": 5,
+      "X": 10,
+      "L": 50,
+      "C": 100,
+      "D": 500,
+      "M": 1000
+  };
+  var result = 0;
+
+  for (var i = 0; i < roman.length; i++) {
+      if (romanToNumber[roman[i]] < romanToNumber[roman[i + 1]]) {
+          result -= romanToNumber[roman[i]];
+      } else {
+          result += romanToNumber[roman[i]];
+      }
+  }
+
+  return result;
+}
+
+function goToFacebook() {
+  window.open("https://www.facebook.com/profile.php?id=61558568662712", "_blank");
 }
